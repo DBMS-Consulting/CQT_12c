342c36f08224c258c1724818641e5001a481f490
hpothuri <harish.pvr@hotmail.com>
2016-07-04 08:20:04 +0530

    CQT Model

---
+++ Model/src/com/dbms/csmq/model/constants/ModelConstants.java
@@ -0,0 +1,29 @@
+package com.dbms.csmq.model.constants;
+
+public class ModelConstants {
+    public ModelConstants() {
+        super();
+    }
+    public static final String NAME_IN_USE_ERROR = "ORA-20111";
+    public static final String INVALID_PROMOTION_ERROR = "ORA-20010";
+    public static final String ACTIVATION_ERRORS = "ORA-20997";
+    public static final String CHECK_MODE_ERRORS = "ORA-20998";
+    public static final String INVALID_PROMOTION_SEQUENCE_ERROR = "ORA-20991";
+    public static final String PROMOTION_DEPENDENCY_ERROR = "ORA-20019";
+    public static final String INVALID_STATE_CHANGE_ERROR = "ORA-20901";
+    public static final String INVALID_STATE_CHANGE_FROM_PENDING_TO_DRAFT_ERROR = "ORA-20101";
+    public static final String RECORD_LOCKED_ERROR = "ORA-20000";    
+    public static final String MUST_BE_NMQ_OR_SMQ_ERROR = "ORA-20902";
+    public static final String GENERIC_ACTIVATION_ERROR = "ORA-20903";
+    public static final String DATABASE_CONFIGURATION_ERROR = "ORA-20992";
+    public static final String NAME_IN_USE_ERROR_NEW = "ORA-20010";
+    
+    public static final String SUCCESS = "Success";
+    public static final String FAILURE = "Failure";
+    public static final String YES = "Y";
+    public static final String NO = "N";
+    public static final String STATE_PENDING_IMPACT_ASSESSMENT = "Pending Impact Assessment";
+    public static final String ACTIVE_ACTIVITY_STATUS = "A";
+    public static final String INACTIVE_ACTIVITY_STATUS = "I";
+    public static final String DELETE_DURING_IMPACT_ANALYSYS = "DELETE_DURING_IMPACT_ANALYSYS";
+}
